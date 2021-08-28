c93a69579fb74e4cbd70de5e84d3112eea4698e0
Muhammad Iqbal <rizki.andrian45@gmail.com>
2021-08-28 11:07:10 +0700

    Move to includes

--- src/website/api/absent.php
+++ src/website/api/absent.php
@@ -1,6 +1,7 @@
 <?php
 		
 	include $_SERVER['DOCUMENT_ROOT'].'/includes/users/class.user.php';
+	include $_SERVER['DOCUMENT_ROOT'].'/includes/dates.php';
 
 	$user = new User();
 
@@ -13,21 +14,6 @@
 		echo "0";
 		die();
 	}
-
-	$date = array();
-	$date['now_timestamp'] = strtotime(date("Y-m-d H:i:s"));
-	$date['subuh'] = strtotime(date("Y-m-d 00:00:00"));
-	$date['now'] = date("Y-m-d H:i:s");
-	$date['morning'] = date("Y-m-d 07:00:00");
-	$date['morning_timestamp'] = strtotime($date['morning']);
-	$date['morning_late'] = strtotime("+20 minutes", strtotime($date['morning']));
-	$date['center_no_1'] = date("Y-m-d 12:00:00");
-	$date['center'] = strtotime($date['center_no_1']);
-	$date['afternoon'] = date("Y-m-d 17:00:00");
-	$date['afternoon_timestamp'] = strtotime($date['afternoon']);
-	$date['afternoon_late'] = strtotime("+20 minutes", strtotime($date['afternoon']));
-	$date['center_no_2'] = date("Y-m-d 23:59:59");
-	$date['center_night'] = strtotime($date['center_no_2']);
 
 	$conn = db();
 
